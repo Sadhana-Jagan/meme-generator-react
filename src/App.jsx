733d02514170af52456c29d170e8f2da0ba87b94
Sadhana-Jagan <sadhanajagan2004@gmail.com>
2025-04-06 19:02:10 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from "./components/Header.jsx"
+import MainComponent from "./components/Main.jsx"
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <MainComponent />
+    </>
+  )
+}
+
+export default App
